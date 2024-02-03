d494f0d4b0edb48be5f90f003c1c3346adc1ccd2
adarsh2508singh <adarshsingh2508@gmail.com>
2024-02-03 15:32:53 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
 function App() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/students')
+      .then((response) => response.json())
+      .then((data) => setStudents(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+console.log(students);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Student Data</h1>
+      <table >
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Nationality</th>
+           
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student) => (
+            <tr key={student.id}>
+              <td>{student.id}</td>
+              <td>{student.first_name}</td>
+              <td>{student.last_name}</td>
+              <td>{student.email}</td>
+              <td>{student.nationality}</td>
+             
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
